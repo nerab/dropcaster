911ddcb5d54819a7bef67430b90f5be27b780104
Nicolas E. Rabenau <nerab@gmx.at>
2011-10-23 22:36:16 +0200

    Add a sample for HTML output template (e.g. --channel-template templates/channel.html.erb)

--- lib/dropcaster/channel.rb
+++ lib/dropcaster/channel.rb
@@ -9,6 +9,8 @@ module Dropcaster
     include ERB::Util # for h() in the ERB template
     include HashKeys
 
+    STORAGE_UNITS = %w(Byte KB MB GB TB)
+
     # Instantiate a new Channel object. +sources+ must be present and can be a String or Array
     # of Strings, pointing to a one or more directories or MP3 files.
     #
@@ -100,7 +102,38 @@ module Dropcaster
 
       all_items.sort{|x, y| y.pub_date <=> x.pub_date}
     end
+    
+    # from http://stackoverflow.com/questions/4136248
+    def humanize_time(secs)
+      [[60, :s], [60, :m], [24, :h], [1000, :d]].map{ |count, name|
+        if secs > 0
+          secs, n = secs.divmod(count)
+          "#{n.to_i}#{name}"
+        end
+      }.compact.reverse.join(' ')
+    end
+    
+    # Fixed version of https://gist.github.com/260184
+    def humanize_size(number)
+      return nil if number.nil?
 
+      storage_units_format = '%n %u'
+
+      if number.to_i < 1024
+        unit = number > 1 ? 'Bytes' : 'Byte'
+        return storage_units_format.gsub(/%n/, number.to_i.to_s).gsub(/%u/, unit)
+      else
+        max_exp  = STORAGE_UNITS.size - 1
+        number   = Float(number)
+        exponent = (Math.log(number) / Math.log(1024)).to_i # Convert to base 1024
+        exponent = max_exp if exponent > max_exp # we need this to avoid overflow for the highest unit
+        number  /= 1024 ** exponent
+
+        unit = STORAGE_UNITS[exponent]
+        return storage_units_format.gsub(/%n/, number.to_i.to_s).gsub(/%u/, unit)
+      end
+    end
+    
   private
     def add_files(src)
       if File.directory?(src)
