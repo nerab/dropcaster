87d63f42b048da72d5a21ba93a4180be4e06e169
Nicholas E. Rabenau <nerab@gmx.at>
2015-06-07 18:18:53 +0200

    Prepare for publication with Travis

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+cd _site
+
+git init
+git config user.name "Nicholas E. Rabenau"
+git config user.email "nerab@gmx.at"
+
+git remote add upstream "https://$GH_TOKEN@github.com/nerab/dropcaster.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+# echo "dropcaster.net" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
